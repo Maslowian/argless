2b360601cbe4ea550601d813762c80a7ad234d97
maslowian <123524393+maslowian@users.noreply.github.com>
2026-02-20 20:52:43 +0000

    Generated single include header

--- single_include/argless.hpp
+++ single_include/argless.hpp
@@ -2401,9 +2401,6 @@ struct _derive_impl<false, t>
 	inline constexpr operator t&() { return m_value; }
 	inline constexpr operator const t&() const { return m_value; }
 
-	inline constexpr auto& pure_cast() { return m_value; }
-	inline constexpr const auto& pure_cast() const { return m_value; }
-
 private:
 	t m_value;
 };
@@ -3527,6 +3524,7 @@ _ARGLESS_CORE_END
 
 #if defined(ARGLESS_STDH_VECTOR) || defined(_GLIBCXX_VECTOR) || defined(_LIBCPP_VECTOR) || defined(_VECTOR_) || \
 	defined(ARGLESS_STDH_LIST) || defined(_GLIBCXX_LIST) || defined(_LIBCPP_LIST) || defined(_LIST_) || \
+	defined(ARGLESS_STDH_FORWARD_LIST) || defined(_GLIBCXX_FORWARD_LIST) || defined(_LIBCPP_FORWARD_LIST) || defined(_FORWARD_LIST_) || \
 	defined(ARGLESS_STDH_DEQUE) || defined(_GLIBCXX_DEQUE) || defined(_LIBCPP_DEQUE) || defined(_DEQUE_) || \
 	defined(ARGLESS_STDH_STACK) || defined(_GLIBCXX_STACK) || defined(_LIBCPP_STACK) || defined(_STACK_) || \
 	defined(ARGLESS_STDH_QUEUE) || defined(_GLIBCXX_QUEUE) || defined(_LIBCPP_QUEUE) || defined(_QUEUE_)
@@ -3537,6 +3535,10 @@ _ARGLESS_CORE_END
 
 #if	defined(ARGLESS_STDH_LIST) || defined(_GLIBCXX_LIST) || defined(_LIBCPP_LIST) || defined(_LIST_)
 #include <list>
+#endif
+
+#if	defined(ARGLESS_STDH_FORWARD_LIST) || defined(_GLIBCXX_FORWARD_LIST) || defined(_LIBCPP_FORWARD_LIST) || defined(_FORWARD_LIST_)
+#include <forward_list>
 #endif
 
 #if defined(ARGLESS_STDH_DEQUE) || defined(_GLIBCXX_DEQUE) || defined(_LIBCPP_DEQUE) || defined(_DEQUE_)
@@ -3603,6 +3605,38 @@ struct parser<t<t_t, t_st>>
 	template <typename char_t>
 	static constexpr inline auto name = array_wrap_name<type_name<t_t, char_t>()>() + str_from<char_t, "[]">();
 };
+
+#if	defined(ARGLESS_STDH_FORWARD_LIST) || defined(_GLIBCXX_FORWARD_LIST) || defined(_LIBCPP_FORWARD_LIST) || defined(_FORWARD_LIST_)
+template <typename t_t, typename t_st>
+struct parser<std::forward_list<t_t, t_st>>
+{
+	using type = std::forward_list<t_t, t_st>; 
+
+	template <typename char_t>
+	static inline parse_result<type, char_t> parse(args<char_t>& args)
+	{
+		type value;
+		auto it = value.before_begin();
+
+		while (true)
+		{
+			auto result = parser<t_t>::template parse<char_t>(args);
+			if (!result.is_valid_no_default())
+				break;
+
+			it = value.insert_after(it, std::move(result).get());
+		}
+
+		if (value.empty())
+			return make_default(std::move(value));
+		else
+			return value;
+	}
+
+	template <typename char_t>
+	static constexpr inline auto name = array_wrap_name<type_name<t_t, char_t>()>() + str_from<char_t, "[]">();
+};
+#endif
 
 _ARGLESS_CORE_END
 
